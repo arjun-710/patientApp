2a7428908c5ce4c32dff45c46216ced9a33dcd21
Arjun <arjunkhare71@gmail.com>
2022-07-10 01:28:25 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:patient_app/screens/LandingPage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Patient App',
+      theme: ThemeData(
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/landing',
+      routes: {
+        '/landing': (context) => LandingPage(),
+      },
+    );
+  }
+}
---
+++ lib/screens/LandingPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
