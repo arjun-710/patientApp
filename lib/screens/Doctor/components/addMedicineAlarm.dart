4882705a6e67db5017f3ec34009b13b99e5de3e2
Arihant Jain <66168181+arihant-jain-09@users.noreply.github.com>
2022-07-22 13:42:42 +0530

    add medicine UI

--- lib/screens/Doctor/components/addMedicineAlarm.dart
+++ lib/screens/Doctor/components/addMedicineAlarm.dart
@@ -3,8 +3,11 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:patient_app/components/CustomText.dart';
+import 'package:patient_app/components/CustomTextButton.dart';
+import 'package:patient_app/components/CustomTextField.dart';
 import 'package:patient_app/constants.dart';
 import 'package:patient_app/screens/Doctor/components/customInfiniteScroll.dart';
+import 'package:patient_app/utils/colors_util.dart';
 // import 'package:patient_app/screens/Doctor/components/weekDays.dart';
 
 class AddMedicineAlarm extends StatefulWidget {
@@ -16,7 +19,33 @@ class AddMedicineAlarm extends StatefulWidget {
 
 class _AddMedicineAlarmState extends State<AddMedicineAlarm> {
   bool isAm = true;
-  List<int> allHours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  List<int> allHours = [
+    00,
+    01,
+    02,
+    03,
+    04,
+    05,
+    06,
+    07,
+    08,
+    09,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24
+  ];
   List<String> list = ['S', 'M', 'T', 'W', 'Th', 'F', 'S'];
 
   List<int> allMinutes = [
@@ -90,6 +119,8 @@ class _AddMedicineAlarmState extends State<AddMedicineAlarm> {
   bool isThu = false;
   bool isFri = false;
   bool isSat = false;
+  TextEditingController medName = TextEditingController();
+  TextEditingController medQuantity = TextEditingController();
 
   void getHourIdx(idx) {
     this.setState(() {
@@ -109,56 +140,161 @@ class _AddMedicineAlarmState extends State<AddMedicineAlarm> {
       child: Scaffold(
           body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-        child: Column(
-          children: [
-            SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SvgPicture.asset(kLeft),
+                    Text("Patient",
+                        style:
+                            TextStyle(fontSize: 32, fontWeight: kh3FontWeight)),
+                    SvgPicture.asset(kDelete)
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SvgPicture.asset(kLeft),
-                  Text("Patient",
-                      style:
-                          TextStyle(fontSize: 32, fontWeight: kh3FontWeight)),
-                  SvgPicture.asset(kDelete)
+                  CustomInfinteScroll(
+                      list: allHours,
+                      callback: getHourIdx,
+                      initialScroll: 65.0),
+                  SizedBox(width: 15),
+                  H2Text(text: ":"),
+                  SizedBox(width: 15),
+                  CustomInfinteScroll(
+                      list: allMinutes,
+                      callback: getMinuteIdx,
+                      initialScroll: 65),
                 ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CustomInfinteScroll(list: allHours, callback: getHourIdx),
-                SizedBox(width: 10),
-                H2Text(text: ":"),
-                SizedBox(width: 10),
-                CustomInfinteScroll(list: allMinutes, callback: getMinuteIdx),
-              ],
-            ),
-            SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AmPm(
-                    text: "AM",
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AmPm(
+                      text: "AM",
+                      onTap: () {
+                        this.setState(() {
+                          isAm = true;
+                        });
+                      },
+                      isAm: isAm),
+                  AmPm(
+                      text: "PM",
+                      onTap: () {
+                        this.setState(() {
+                          isAm = false;
+                        });
+                      },
+                      isAm: !isAm),
+                ],
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
                     onTap: () {
-                      this.setState(() {
-                        isAm = true;
+                      setState(() {
+                        isSun = !isSun;
                       });
                     },
-                    isAm: isAm),
-                AmPm(
-                    text: "PM",
+                    child: Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            color: isSun ? kPatCardColor : HexColor("EFF6FC"),
+                            borderRadius: BorderRadius.circular(kBorderRadius)),
+                        child: Text("S")),
+                  ),
+                  SizedBox(width: 10),
+                  GestureDetector(
                     onTap: () {
-                      this.setState(() {
-                        isAm = false;
+                      setState(() {
+                        isMon = !isMon;
                       });
                     },
-                    isAm: !isAm),
-              ],
-            ),
-            SizedBox(height: 30),
-          ],
+                    child: Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            color: isMon ? kPatCardColor : HexColor("EFF6FC"),
+                            borderRadius: BorderRadius.circular(kBorderRadius)),
+                        child: Text("M")),
+                  ),
+                  SizedBox(width: 10),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isTue = !isTue;
+                      });
+                    },
+                    child: Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            color: isTue ? kPatCardColor : HexColor("EFF6FC"),
+                            borderRadius: BorderRadius.circular(kBorderRadius)),
+                        child: Text("T")),
+                  ),
+                  SizedBox(width: 10),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isThu = !isThu;
+                      });
+                    },
+                    child: Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            color: isThu ? kPatCardColor : HexColor("EFF6FC"),
+                            borderRadius: BorderRadius.circular(kBorderRadius)),
+                        child: Text("Th")),
+                  ),
+                  SizedBox(width: 10),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isFri = !isFri;
+                      });
+                    },
+                    child: Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            color: isFri ? kPatCardColor : HexColor("EFF6FC"),
+                            borderRadius: BorderRadius.circular(kBorderRadius)),
+                        child: Text("F")),
+                  ),
+                  SizedBox(width: 10),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isSat = !isSat;
+                      });
+                    },
+                    child: Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            color: isSat ? kPatCardColor : HexColor("EFF6FC"),
+                            borderRadius: BorderRadius.circular(kBorderRadius)),
+                        child: Text("S")),
+                  ),
+                ],
+              ),
+              SizedBox(height: 40),
+              CustomTextField(controller: medName, hintText: "medicine Name"),
+              SizedBox(height: 20),
+              CustomTextField(controller: medQuantity, hintText: "Quantity"),
+              SizedBox(height: 40),
+              CustomTextButton(
+                  onTap: () {},
+                  label: "Save Alarm",
+                  children: SizedBox.shrink())
+            ],
+          ),
         ),
       )),
     );
@@ -181,10 +317,9 @@ class AmPm extends StatelessWidget {
           onTap: onTap,
           child: Container(
             decoration: BoxDecoration(
-                border: Border.all(width: 1.0),
                 borderRadius: BorderRadius.circular(kBorderRadius),
-                color: isAm ? kPatCardColor : Colors.white),
-            padding: EdgeInsets.all(8.0),
+                color: isAm ? kPatCardColor : HexColor("EFF6FC")),
+            padding: EdgeInsets.all(12),
             child: Text(
               text,
               style: TextStyle(fontSize: 20),
